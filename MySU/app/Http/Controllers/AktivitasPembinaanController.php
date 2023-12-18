5f8f2aaa63e015a04d81bf8ba836570e6ae93021
Rifqi Abdulaziz <74664919+RoneAllza@users.noreply.github.com>
2023-12-18 09:59:27 +0700

    init semua fitur

---
+++ MySU/app/Http/Controllers/AktivitasPembinaanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AktivitasPembinaan;
+use Illuminate\Http\Request;
+
+class AktivitasPembinaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AktivitasPembinaan $aktivitasPembinaan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AktivitasPembinaan $aktivitasPembinaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AktivitasPembinaan $aktivitasPembinaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AktivitasPembinaan $aktivitasPembinaan)
+    {
+        //
+    }
+}
---
+++ MySU/app/Http/Controllers/KegiatanTahfizhController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KegiatanTahfizh;
+use Illuminate\Http\Request;
+
+class KegiatanTahfizhController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KegiatanTahfizh $kegiatanTahfizh)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KegiatanTahfizh $kegiatanTahfizh)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KegiatanTahfizh $kegiatanTahfizh)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KegiatanTahfizh $kegiatanTahfizh)
+    {
+        //
+    }
+}
